de7c57f72c9b286beff13a2348b6875ee0ff8162
Greg Furstenwerth <furstenwerth@mac.com>
2020-10-31 09:17:26 -0600

    add freebsd upgrade
    Implementation loosely based on mkjail.sh and poudriere jail creation. Components are missing for major releases. Minor releases do not require kernel to be extracted then reboot then finalize. That is a more complicated procedure that needs some planning.

--- salt/modules/freebsd_update.py
+++ salt/modules/freebsd_update.py
@@ -1,236 +1,138 @@
 # -*- coding: utf-8 -*-
 """
-Support for freebsd-update utility on FreeBSD.
-
-.. versionadded:: 2017.7.0
-
-:maintainer:    George Mamalakis <mamalos@gmail.com>
-:maturity:      new
-:platform:      FreeBSD
+The release upgrade module for FreeBSD
 """
 
+# Import python libs
 from __future__ import absolute_import, print_function, unicode_literals
 
-# Import python libs
-import logging
+import os
+import re
+import subprocess
 
 # Import salt libs
-import salt.utils.path
-from salt.exceptions import CommandNotFoundError
-from salt.ext import six
-
-log = logging.getLogger(__name__)
+import salt.utils.args
+import salt.utils.files
+import salt.utils.stringutils
 
 # Define the module's virtual name
 __virtualname__ = "freebsd_update"
-__virtual_aliases__ = ("freebsd-update",)
 
 
 def __virtual__():
     """
-    .. versionadded:: 2016.3.4
-
-    Only work on FreeBSD RELEASEs >= 6.2, where freebsd-update was introduced.
+    Only runs on FreeBSD systems
     """
-    if __grains__["os"] != "FreeBSD":
-        return (
-            False,
-            "The freebsd_update execution module cannot be loaded: only available on FreeBSD systems.",
-        )
-    if float(__grains__["osrelease"]) < 6.2:
-        return (
-            False,
-            "freebsd_update is only available on FreeBSD versions >= 6.2-RELESE",
-        )
-    if "release" not in __grains__["kernelrelease"].lower():
-        return (False, "freebsd_update is only available on FreeBSD RELEASES")
-    return __virtualname__
+    if __grains__["os"] == "FreeBSD":
+        return __virtualname__
+    return (
+        False,
+        "The freebsd_update execution module cannot be loaded: "
+        "only available on FreeBSD systems.",
+    )
 
 
-def _cmd(**kwargs):
+def chflags(host_files):
     """
-    .. versionadded:: 2016.3.4
+    Set chflags on host
 
-    Private function that returns the freebsd-update command string to be
-    executed. It checks if any arguments are given to freebsd-update and appends
-    them accordingly.
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' freebsd_update.chflags <host_files>
     """
-    update_cmd = salt.utils.path.which("freebsd-update")
-    if not update_cmd:
-        raise CommandNotFoundError('"freebsd-update" command not found')
+    switch = ""
+    loclist = [
+        "var/empty",
+        "lib",
+        "bin",
+        "sbin",
+        "usr/bin",
+        "usr/lib",
+        "usr/lib32",
+        "usr/libexec",
+        "usr/sbin",
+        "libexec",
+    ]
 
-    params = []
-    if "basedir" in kwargs:
-        params.append("-b {0}".format(kwargs["basedir"]))
-    if "workdir" in kwargs:
-        params.append("-d {0}".format(kwargs["workdir"]))
-    if "conffile" in kwargs:
-        params.append("-f {0}".format(kwargs["conffile"]))
-    if "force" in kwargs:
-        params.append("-F")
-    if "key" in kwargs:
-        params.append("-k {0}".format(kwargs["key"]))
-    if "newrelease" in kwargs:
-        params.append("-r {0}".format(kwargs["newrelease"]))
-    if "server" in kwargs:
-        params.append("-s {0}".format(kwargs["server"]))
-    if "address" in kwargs:
-        params.append("-t {0}".format(kwargs["address"]))
-
-    if params:
-        return "{0} {1}".format(update_cmd, " ".join(params))
-    return update_cmd
+    for data in loclist:
+        data = "{0}{1}".format("/", data)
+        filelist = __salt__["file.readdir"](data)
+        for files in filelist:
+            truepath = '{0}/{1}'.format(data, host_files)
+            __salt__["chflags.change"](switch, "noschg", truepath)
 
 
-def _wrapper(orig, pre="", post="", err_=None, run_args=None, **kwargs):
+def etc_resolve():
     """
-    Helper function that wraps the execution of freebsd-update command.
+    resolve etc
 
-    orig:
-        Originating function that called _wrapper().
+    CLI Example:
 
-    pre:
-        String that will be prepended to freebsd-update command.
+    .. code-block:: bash
 
-    post:
-        String that will be appended to freebsd-update command.
-
-    err_:
-        Dictionary on which return codes and stout/stderr are copied.
-
-    run_args:
-        Arguments to be passed on cmd.run_all.
-
-    kwargs:
-        Parameters of freebsd-update command.
+        salt '*' freebsd_update.etc_resolve
     """
-    ret = ""  # the message to be returned
-    cmd = _cmd(**kwargs)
-    cmd_str = " ".join([x for x in (pre, cmd, post, orig)])
-    if run_args and isinstance(run_args, dict):
-        res = __salt__["cmd.run_all"](cmd_str, **run_args)
+    __salt__["cmd.run"]("etcupdate")
+    __salt__["cmd.run"]("etcupdate resolve")
+
+
+def base_upgrade(host_files, version):
+    """
+    host base upgrade
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' freebsd_upgrade.base_upgrade <host_files> <version>
+    """
+    __salt__["archive.tar"]("xzf", '{0}/{1}/kernel.txz'.format(host_files, str(version)), dest="/")
+    __salt__["archive.tar"]("xzf", '{0}/{1}/base.txz'.format(host_files, str(version)), dest="/", exclude="etc")
+
+
+def lib32_upgrade(host_files, version):
+    """
+    host lib32 upgrade
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' freebsd_upgrade.lib32_upgrade <host_files> <version>
+    """
+    if __salt__["archive.tar"]("xzf", "{0}/{1}/lib32.txz".format(host_files, str(version)), dest="/") is True:
+        return True
     else:
-        res = __salt__["cmd.run_all"](cmd_str)
-
-    if isinstance(err_, dict):  # copy return values if asked to
-        for k, v in six.itermitems(res):
-            err_[k] = v
-
-    if "retcode" in res and res["retcode"] != 0:
-        msg = " ".join([x for x in (res["stdout"], res["stderr"]) if x])
-        ret = 'Unable to run "{0}" with run_args="{1}". Error: {2}'.format(
-            cmd_str, run_args, msg
-        )
-        log.error(ret)
-    else:
-        try:
-            ret = res["stdout"]
-        except KeyError:
-            log.error(
-                "cmd.run_all did not return a dictionary with a key named 'stdout'"
-            )
-    return ret
+        return False
 
 
-def fetch(**kwargs):
+def manage(host_files, version, lib32, src, pass_opt):
     """
-    .. versionadded:: 2016.3.4
+    manage FreeBSD release
 
-    freebsd-update fetch wrapper. Based on the currently installed world and the
-    configuration options set, fetch all available binary updates.
+    snapshot, lib32, src are true/false
 
-    kwargs:
-        Parameters of freebsd-update command.
+    host_files is location of txz files
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' freebsd_upgrade.manage <host_files> <version> <lib32> <src>
     """
-    # fetch continues when no controlling terminal is present
-    pre = ""
-    post = ""
-    run_args = {}
-    if float(__grains__["osrelease"]) >= 10.2:
-        post += "--not-running-from-cron"
-    else:
-        pre += " env PAGER=cat"
-        run_args["python_shell"] = True
-    return _wrapper("fetch", pre=pre, post=post, run_args=run_args, **kwargs)
+    host_version = __salt__["grains.get"]("release_version")
 
+    if host_version != version and pass_opt is False:
 
-def install(**kwargs):
-    """
-    .. versionadded:: 2016.3.4
+        __salt__["freebsd_update.chflags"]("/")
 
-    freebsd-update install wrapper. Install the most recently fetched updates or
-    upgrade.
+        if __salt__["freebsd_update.base_upgrade"](host_files, version) is True:
 
-    kwargs:
-        Parameters of freebsd-update command.
-    """
-    return _wrapper("install", **kwargs)
+            if lib32 is True:
+                __salt__["freebsd_update.lib32_upgrade"](host_files, version)
 
-
-def rollback(**kwargs):
-    """
-    .. versionadded:: 2016.3.4
-
-    freebsd-update rollback wrapper. Uninstalls the most recently installed
-    updates.
-
-    kwargs:
-        Parameters of freebsd-update command.
-    """
-    return _wrapper("rollback", **kwargs)
-
-
-def update(**kwargs):
-    """
-    .. versionadded:: 2016.3.4
-
-    Command that simplifies freebsd-update by running freebsd-update fetch first
-    and then freebsd-update install.
-
-    kwargs:
-        Parameters of freebsd-update command.
-    """
-    stdout = {}
-
-    for mode in ("fetch", "install"):
-        err_ = {}
-        ret = _wrapper(mode, err_=err_, **kwargs)
-        if "retcode" in err_ and err_["retcode"] != 0:
-            return ret
-        if "stdout" in err_:
-            stdout[mode] = err_["stdout"]
-    return "\n".join(["{0}: {1}".format(k, v) for (k, v) in six.iteritems(stdout)])
-
-
-def ids(**kwargs):
-    """
-    .. versionadded:: 2016.3.4
-
-    freebsd-update IDS wrapper function. Compares the system against a "known
-    good" index of the installed release.
-
-    kwargs:
-        Parameters of freebsd-update command.
-    """
-    return _wrapper("IDS", **kwargs)
-
-
-def upgrade(**kwargs):
-    """
-    .. versionadded:: 2016.3.4
-
-    Dummy function used only to print a message that upgrade is not available.
-    The reason is that upgrade needs manual intervention and reboot, so even if
-    used with:
-
-       yes | freebsd-upgrade -r VERSION
-
-    the additional freebsd-update install that needs to run after the reboot
-    cannot be implemented easily.
-
-    kwargs:
-        Parameters of freebsd-update command.
-    """
-    msg = "freebsd-update upgrade not yet implemented."
-    log.warning(msg)
-    return msg
+    __salt__["grains.set"]("release_version", val=version)
+    __salt__["saltutil.refresh_grains"]
+    return True
